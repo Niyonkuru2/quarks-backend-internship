f93d57b620094f488b8c8ed8fcfcde438807b84e
niyokuru2 <niyonkurusam5@gmail.com>
2025-06-18 16:02:15 +0200

    userController.js  code logic

---
+++ quarks-apis/controllers/userController.js
@@ -0,0 +1,26 @@
+import { validateUserInput } from "../utils/validateUser";
+import { createUser, getUserById } from "../models/validateUser.js"
+
+//function to create user
+function createUser(req,res){
+
+// validate user input
+const error = validateUserInput(req,body);
+if (error) return res.status(400).json({error});
+const {name,email} = req.body;
+//create user and save in memory
+const user = createUser(name,email);
+
+return res.status(201).json(user);
+}
+
+//function to get user details depend on id of the user
+function getUser(req, res) {
+  const user = getUserById(req.params.id);
+  if (!user) return res.status(404).json({ success:'false',message:'User not found' });
+
+  // return user information match with specified id
+  return res.json(user);
+}
+
+export { createUser, getUser };
