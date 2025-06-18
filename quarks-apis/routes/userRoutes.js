2b4b197d14c24d773b54acc8f933c616726bcf5c
niyokuru2 <niyonkurusam5@gmail.com>
2025-06-18 16:20:34 +0200

    userRoute.js register route

--- quarks-apis/routes/userRoutes.js
+++ quarks-apis/routes/userRoutes.js
@@ -4,7 +4,7 @@ import { registerUser, getUser } from '../controllers/userController.js';
 const useRouter = express.Router();
 
 // Defining users routes for defined controllers
-useRouter.post('/',registerUser);
+useRouter.post('/register',registerUser);
 useRouter.get('/:id',getUser);
 
 export {useRouter};
