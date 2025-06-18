e692da32609d77004d95058808880a22086d170e
niyokuru2 <niyonkurusam5@gmail.com>
2025-06-18 16:11:19 +0200

    app.js

--- quarks-apis/app.js
+++ quarks-apis/app.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import { useRouter } from './routes/userRoutes.js';
 const app = express();
 app.use(express.json()); // Allowing Json Body parsing
 
+//Routes
+app.use('/users',useRouter)
 //Get mesage from server of success running on browser get  
 app.get('/',(req,res)=>{
     res.send('Express server is Running')
